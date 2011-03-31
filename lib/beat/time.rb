875b6164f66653c2a49503912bdad2719569b83c
TJ Vanderpoel <tj@rubyists.com>
2011-03-31 12:13:51 -0500

    added #beat_date_string and #beat_datetime_string

--- lib/beat/time.rb
+++ lib/beat/time.rb
@@ -7,5 +7,13 @@ module Beat
     def beats
       (hour * (1000.0/24.0)) + (min * (1000.0/1440.0)) + (sec * (1000.0/86400.0))
     end
+
+    def beat_date_string
+      strftime("%Y-%mm-%dd")
+    end
+
+    def beat_datetime_string
+      strftime("%Y-%mm-%dd@#{"%0.2f" % beats}")
+    end
   end
 end
